2287073c32d51a428492d71b6fa1aa1094e47466
Barry Stovall <bw.stovall@gmail.com>
2025-08-23 15:33:15 -0700

    missing directory that PHPUnit expected to exist

---
+++ tests/Integration/ServiceProviderTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace DangerWayne\Specification\Tests\Integration;
+
+use DangerWayne\Specification\Facades\Specification;
+use DangerWayne\Specification\Specifications\Builders\SpecificationBuilder;
+use DangerWayne\Specification\Tests\TestCase;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
+
+class ServiceProviderTest extends TestCase
+{
+    public function test_service_provider_registers_specification_builder(): void
+    {
+        $specification = $this->app->make('specification');
+
+        $this->assertInstanceOf(SpecificationBuilder::class, $specification);
+    }
+
+    public function test_facade_resolves_to_specification_builder(): void
+    {
+        $builder = Specification::getFacadeRoot();
+
+        $this->assertInstanceOf(SpecificationBuilder::class, $builder);
+    }
+
+    public function test_collection_macro_is_registered(): void
+    {
+        $this->assertTrue(Collection::hasMacro('whereSpecification'));
+    }
+
+    public function test_builder_macro_is_registered(): void
+    {
+        $this->assertTrue(Builder::hasGlobalMacro('whereSpecification'));
+    }
+}
