a11fe24bd2c682cf1c147d8fe9c7fa0a4c5a1ae0
Barry Stovall <bw.stovall@gmail.com>
2025-08-23 17:17:52 -0700

    Restructured ServiceProvider macros to be more explicit

--- src/Providers/SpecificationServiceProvider.php
+++ src/Providers/SpecificationServiceProvider.php
@@ -33,14 +33,14 @@ class SpecificationServiceProvider extends ServiceProvider
     {
         // Add Collection macro
         Collection::macro('whereSpecification', function ($specification) {
-            /** @var Collection $this */
-            return $this->filter(fn ($item) => $specification->isSatisfiedBy($item));
+            $collection = $this;
+            return $collection->filter(fn ($item) => $specification->isSatisfiedBy($item));
         });
 
         // Add Builder macro
         Builder::macro('whereSpecification', function ($specification) {
-            /** @var Builder $this */
-            return $specification->toQuery($this);
+            $builder = $this;
+            return $specification->toQuery($builder);
         });
     }
 }
